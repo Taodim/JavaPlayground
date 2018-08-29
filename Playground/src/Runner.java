7c6dc8e82aac99a1f95010eb5447d017124d2502
tdao <tdao@ROC-11.sd.spawar.navy.mil>
2018-08-29 12:56:33 -0700

    Initial Commit

---
+++ Playground/src/Runner.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import sandboxes.Sand;
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		//Create new sand obj
+		Sand obj = new Sand();
+		
+		//Call Speak
+		obj.Speak();
+		
+		//Print array toString differences
+		System.out.println(Arrays.toString(obj.array1));
+		System.out.println(obj.array1.toString());
+	}
+
+}
---
+++ Playground/src/sandboxes/Sand.java
@@ -0,0 +1,14 @@
+package sandboxes;
+
+public class Sand {
+	//VARs
+	public int array1[] = {1,2,3,4,5}; 
+	
+	//METHODS
+	public void Speak() { 
+		//print something
+		System.out.println("Hello!");
+	}
+	
+	
+}
